f89a5ba93a156d29c9e558ba2625a94b85479286
GreatestFool <3003484+GreatestFool@users.noreply.github.com>
2021-06-03 19:23:54 +0200

    Init develop

--- pack-manager.py
+++ pack-manager.py
@@ -19,12 +19,14 @@
 # * Arrays to loop through folder to pick out targets for Hard symlinks
 # * Arrays to loop through folders for Junction symlinking
 #
-# Look at twitch-export-builder for how to replicate behaviour. https://github.com/Gaz492/twitch-export-builder/ and https://github.com/Gaz492/twitch-export-builder/.
+# Look at twitch-export-builder for how to replicate behaviour. https://gaz492.github.io/twitch-export-builder/usage and https://github.com/Gaz492/twitch-export-builder/.
 #   NOTE There's also nothing stopping me from simply using the binaries by launching them via. this script or anything. Something to consider.
-# This is a tad older and is made in Python, but it may still be helpful. https://github.com/tcheinen/twitch-modpack-exporter.
+# This is a tad older and is made in Python, but it may still be helpful. https://github.com/cdbbnnyCode/modpack-installer.
+# NOTE https://github.com/NillerMedDild/ModpackUploader
 #
 # This also looks like it is pretty much what I wanted, for the management side of installing mods and such and aside from the auto uploading releases part. https://github.com/dizzyd/mcdex
 # NOTE Being a cli tool, this may also be rather helpful to use *in* this script. E.g. looping through an array that is piped to the install of mcdex... That could work. Will investigate.
+# REMARK Didn't really work that well. Didn't bother testing *too* much, but it had errors from the beginning and wouldn't add mods without doing the path to the modpack. Too cumbersome.
 #
 # ---
 #
@@ -53,7 +55,81 @@
 # ---
 #
 # Example processing script
-#
+
+import json
+
+#‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾#
+#                   Modlists                    #
+#_______________________________________________#
+
+# ["projectID", "fileID"]
+# ["projectID"] # If a fileID is not specified, use latest available on curseforge
+# Alternative: mods_performance.append(["354143", "3329629"])
+# mods_performance = [
+#     ["354143", "3329629"]  # Performant : v2-5-3.64m
+# ]
+# 
+# mods_fixes = [
+#     ["fixes_projectID", "fixes_fileID"]
+# ]
+# 
+# mods_QoL = [
+#     ["QoL_projectID", "QoL_fileID"]
+# ]
+# 
+# mods_aesthetic = [
+#     ["aesthetic_projectID", "aesthetic_fileID"]
+# ]
+# 
+# mods_utility = [
+#     ["utility_projectID", "utility_fileID"]
+# ]
+#  
+# mods_general = [
+#     ["general_projectID", "general_fileID"]
+# ]
+
+# test = []
+# test.append(mods_fixes)
+# test.append(mods_utility)
+
+# modlist = [*mods_performance, *mods_fixes, *mods_utility, *mods_general]
+# modlist = mods_performance + mods_fixes + mods_QoL + mods_aesthetic + mods_utility + mods_general
+
+mods_performance = '[ "Performant": { "projectID":354143, "fileID":3329629, "Enabled": "true" }, ' \
+                    '"Example": { "projectID":341453, "fileID":926233, "Enabled": "true" }]'
+
+mods_fixes = '{ "projectID":"fixes_projectID", "fileID":fixes_fileID }'
+
+modlist = mods_performance, mods_fixes
+
+print(modlist["ProjectID"])
+
+#‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾#
+#           modlist exporter function           #
+#_______________________________________________#
+
+# Join the various mod lists
+# Resolve urls and 'owner'
+# Add to modlist following scheme
+
+# Download link https://addons-ecs.forgesvc.net/api/v2/addon/${projectID}/file/${fileID}/download-url
+
+# for x in mods:
+#   add(x)
+
+#‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾#
+#           manifest exporter function          #
+#_______________________________________________#
+
+# Resolve projectID (unless specified in modlists)
+# Specify fileID or grab latest
+# Add to manifest following scheme
+
+#‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾#
+#               Exporter function               #
+#_______________________________________________#
+
 # import shutil
 # import zipfile
 # import os
@@ -61,8 +137,6 @@
 # # Create base import and export folders
 # if not os.path.exists('.exports'):
 #     os.makedirs('.exports')
-# if not os.path.exists('.imports'):
-#     os.makedirs('.imports')
 # 
 # # Create temporary folder for processing new release data.
 # if not os.path.exists('.processing'):
@@ -70,16 +144,16 @@
 # else: # I don't why I added this, but I remember it being for a reason and I'm afraid to remove it, so I'll just leave it...
 #     shutil.rmtree('.processing')
 # 
-# # Extract modpack zip file to '.processing'
-# # TODO currently hardcoded, fix later by making it grab whichever is placed in imports
-# with zipfile.ZipFile('.imports/All+the+Mods+6-1.6.4.zip', 'r') as zip_ref:
-#     zip_ref.extractall('.processing/')
-# 
-# # Copy mod addition configs to '.processing/overrides/config/'
-# shutil.copytree('.remix-patch/config', '.processing/overrides/config', symlinks=False, ignore=shutil.ignore_patterns('.ignore'), dirs_exist_ok=True)
-# 
-# # TODO Modify configuration files for already existing files using hardcoded values
-# # TODO Modify manifest.json and modlist.toml
+# # # Extract modpack zip file to '.processing'
+# # # TODO currently hardcoded, fix later by making it grab whichever is placed in imports
+# # with zipfile.ZipFile('.imports/All+the+Mods+6-1.6.4.zip', 'r') as zip_ref:
+# #     zip_ref.extractall('.processing/')
+# # 
+# # # Copy mod addition configs to '.processing/overrides/config/'
+# # shutil.copytree('.remix-patch/config', '.processing/overrides/config', symlinks=False, ignore=shutil.ignore_patterns('.ignore'), dirs_exist_ok=True)
+# # 
+# # # TODO Modify configuration files for already existing files using hardcoded values
+# # # TODO Modify manifest.json and modlist.toml
 # 
 # # Package '.processing' into a zip file
 # # TODO Make the export name variable and return import name with some additions like a version counter
